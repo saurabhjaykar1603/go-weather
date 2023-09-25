ce108aea1a3fb4abb9649239d6fceebf8f44cbca
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-09-25 11:50:24 +0530

    feat : data fetched from axios libary

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React, { useState } from 'react'
-import axios  from 'axios'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 const App = () => {
-  const [weatherdata , setWeatherData] = useState({})
+  // func for load weatherdata from api
+  async function loadWeatherData() {
+    const respone = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=Mumbai&appid=6c20b7414d083408cadcb2f16f510ca4`
+    );
+    setWeatherData(respone.data);
+  }
+
+  const [weatherdata, setWeatherData] = useState({});
+
+  useEffect(() => {
+    loadWeatherData();
+  }, [weatherdata, setWeatherData]);
+
   return (
     <>
       <h1>Go Weather</h1>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
