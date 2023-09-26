c2b57c669ae3de3b03267fa2657c7df50556cd6b
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-09-26 23:59:52 +0530

    feat : data succecfully delivered

--- src/App.js
+++ src/App.js
@@ -3,24 +3,45 @@ import axios from "axios";
 import "./App.css";
 
 const App = () => {
+  const [weatherData, setWeatherData] = useState({});
+
+  // city
+
+  const [city, setCity] = useState("pune");
+
   // func for load weatherdata from api
   async function loadWeatherData() {
-    const respone = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=Mumbai&appid=6c20b7414d083408cadcb2f16f510ca4`
-    );
-    setWeatherData(respone.data);
+    try{
+     const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=6c20b7414d083408cadcb2f16f510ca4`
+      );
+      setWeatherData(response.data);
+    }
+    catch(error){
+      console.log(error);
+    }
+    
   }
-
-  const [weatherData, setWeatherData] = useState({});
 
   useEffect(() => {
     loadWeatherData();
-  }, [weatherData, setWeatherData]);
+  }, );
+
+  useEffect(() => {
+    loadWeatherData();
+  }, [city]);
 
   return (
     <>
       <div>
-        <h1>Go Weather</h1>
+        <h1>Go Weather {city}</h1>
+        <input
+          type="text"
+          value={city}
+          onChange={(e) => {
+            setCity(e.target.value);
+          }}
+        />
         <p>city : {weatherData?.name}</p>
         <p>Temprature : {(weatherData?.main?.temp - 273).toFixed(2)}°C</p>
         <p>
@@ -28,7 +49,7 @@ const App = () => {
           {weatherData?.weather && weatherData?.weather[0]?.description})
         </p>
 
-        <p>Visibility : { weatherData?.visibility } meters</p>
+        <p>Visibility : {weatherData?.visibility} meters</p>
       </div>
     </>
   );
